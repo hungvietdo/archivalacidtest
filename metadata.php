90e2d149f01d2a3879cced1c6a12c00f02e0c853
Hung Do <hdo@cs.odu.edu>
2016-05-17 22:16:39 -0400

    complete structure

---
+++ metadata.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+   
+
+</head>
+
+<h3>Metadata</h3>
+<ul>
+<li>Reported User-Agent string: <?php echo $_SERVER['HTTP_USER_AGENT']; ?></li>
+<li>Detected User-Agent: <span id="detectedBrowser"></span></li>
+<li>Access Time: <?php echo date('Y-m-d H:i:s'); ?></li>
+</ul>
+
+</body>
+
+</html>
