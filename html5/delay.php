16fd1601bf0fe8737612f7d96ebcfe26b5ecb024
Hung Do <hdo@cs.odu.edu>
2016-06-11 16:56:49 -0400

    delay

---
+++ html5/delay.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+date_default_timezone_set('UTC');
+echo date('h:i:s') . "<br>";
+
+?>
+
+hung
+
+<?php
+
+//sleep for 5 seconds
+sleep(10);
+
+//start again
+echo date('h:i:s');
+?>
+
+</body>
+</html>
