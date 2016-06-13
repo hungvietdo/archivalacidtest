c2f21fba30ed616301f402c4083092a45d68ebe9
Hung Do <hdo@cs.odu.edu>
2016-06-13 17:08:34 -0400

    animation

---
+++ animation/js/source.js
@@ -0,0 +1,37 @@
+var create_blx_54e284395b51ea741400003d_0000001 = function( element ) {
+    return {
+        init: function( options ) {
+            this.element = element;
+            this.element.textillate( options );
+        }
+    }
+}
+
+var options_blx_54e284395b51ea741400003d_0000001 =
+{
+    type: "char",
+    loop: true,
+    minDisplayTime: 1000,
+    in: {
+        effect: "zoomIn",
+        delay: 125,
+        delayScale: 1,
+        shuffle: true,
+        reverse: false
+    },
+    out: {
+        effect: "shake",
+        delay: 0,
+        delayScale: 1,
+        shuffle: true,
+        reverse: false
+    }
+};
+
+var init_blx_54e284395b51ea741400003d_0000001 = function( create, selector, options ) {
+    var text = create( $( selector ) );
+    text.init( options );
+    return text;
+}
+
+$( document ).ready( function() {init_blx_54e284395b51ea741400003d_0000001( create_blx_54e284395b51ea741400003d_0000001, "#blx-54e284395b51ea741400003d-0000001", options_blx_54e284395b51ea741400003d_0000001 ); } );
