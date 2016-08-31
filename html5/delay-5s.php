c5e0b6609101913d7eb154c1023799f6602dfcb3
Hung Do <hung.do@dominionenterprises.com>
2016-08-31 13:25:48 -0400

    change delay.php

---
+++ html5/delay-5s.php
@@ -0,0 +1,19 @@
+<html>
+
+<?php
+
+$seconds = 5;
+
+// current time
+echo "Start time: " . date('h:i:s') . "<br>";
+
+// sleep for 10 seconds
+sleep($seconds);
+
+echo "Sleep for {$seconds} seconds <br>";
+// wake up !
+echo "End time: " . date('h:i:s') . "<br>";
+
+?>
+
+</html>
