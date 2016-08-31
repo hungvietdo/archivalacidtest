50abe4714b4016f5a7e7127ec08308df936ba474
Hung Do <hung.do@dominionenterprises.com>
2016-08-31 10:37:44 -0400

    add delay in php

--- html5/delay.php
+++ html5/delay.php
@@ -1,34 +1,18 @@
-<!DOCTYPE html>
 <html>
-<body>
+
+<p> hung </p>
 
 <?php
-date_default_timezone_set('UTC');
-echo date('h:i:s') . "<br>";
+
+// current time
+echo date('h:i:s') . "\n";
+
+// sleep for 10 seconds
+sleep(6);
+
+// wake up !
+echo date('h:i:s') . "\n";
 
 ?>
 
-<dir id="demo"></dir>
-
-<script type="text/javascript">
-	
-	var delay=6000; //1 second
-
-	var htmltext = "<?php echo 'abc'?>";
-
-	document.addEventListener('DOMContentLoaded',function(){
-	//Test 12 setup
-	var scriptParent = document.getElementById('demo');
-	//scriptParent.innerHTML = htmltext;
-	
-});
-
-	setTimeout(function() {
-			var scriptParent = document.getElementById('demo');
-	scriptParent.innerHTML = htmltext;
-  
-}, delay);
-</script>
-
-</body>
 </html>
