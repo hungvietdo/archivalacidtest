699fdcea38b215456908586249900b4c804d8d15
Hung Do <hung.do@dominionenterprises.com>
2016-09-06 23:24:59 -0400

    level

---
+++ levels/delay.php
@@ -0,0 +1,19 @@
+<html>
+
+<?php
+
+$seconds = $_GET["seconds"];
+
+// current time
+echo "Start time: " . date('h:i:s') . "<br>";
+
+// sleep for 10 seconds
+sleep($seconds);
+
+echo "Sleep for {$seconds} seconds <br>";
+// wake up !
+echo "End time: " . date('h:i:s') . "<br>";
+
+?>
+
+</html>
---
+++ levels/index.html
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<p>
+			This is the main page
+		</p>
+
+		<p>
+			
+			<a href="level1.html">This is link page</a>
+		</p>
+
+	</body>
+</html>
