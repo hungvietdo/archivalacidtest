7dc223f4a3cc0b92fcd1bf170a0ad20c4181a98c
hungdoodu <do.viethung@gmail.com>
2016-09-21 16:33:29 -0400

    edit index

--- levels/index.php
+++ levels/index.php
@@ -7,15 +7,28 @@
 		<h1>
 			Acid Test Main Page
 		</h1>
-
-		<div id="demo">
+		<h3>PHP test</h3>
+		<div>Load for two seconds</div>		
+<div id="demo">
 				<?php 
-		$imgSrc='createImage.php?sleep=6'; 
+		$imgSrc='createImage.php?sleep=2'; 
 		echo('<img src="'.$imgSrc.'"/>');
 
 		?>
 		</div>
 
+			
+		<div>Load for six seconds</div>		
+<div id="demo">
+		<div>		<?php 
+		$imgSrc='createImage.php?sleep=6'; 
+		echo('<img src="'.$imgSrc.'"/>');
+
+		?>
+		</div>
+		
+		<h3>JS testing<h3>
+		
 		<script type="text/javascript">
 		
 		//	document.getElementById("demo").innerHTML =
