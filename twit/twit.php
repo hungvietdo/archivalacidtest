d02ec7505eb03c7b685d1dae9c85e0ab3d1efc0a
Hung Do <hung.do@dominionenterprises.com>
2016-10-05 14:33:10 -0400

    twitter frame

--- twit/twit.php
+++ twit/twit.php
@@ -1,3 +1,31 @@
 
-<iframe border=0 frameborder=0 height=250 width=550 
+<iframe border=0 frameborder=0 height=150 width=300 
  src="http://twitframe.com/show?url=https://twitter.com/JohnOlpoet/status/783728113138229250"></iframe>
+
+
+<iframe border=0 frameborder=0 height=150 width=300  
+ src="http://twitframe.com/show?url=https://twitter.com/phonedude_mln/status/783721103810105344"></iframe>
+
+<iframe border=0 frameborder=0 height=150 width=300 
+ src="http://twitframe.com/show?url=https://twitter.com/weiglemc/status/783675741959516160"></iframe>
+
+<iframe border=0 frameborder=0 height=150 width=300 
+ src="http://twitframe.com/show?url=https://twitter.com/JohnOlpoet/status/783728113138229250"></iframe>
+
+
+<iframe border=0 frameborder=0 height=150 width=300  
+ src="http://twitframe.com/show?url=https://twitter.com/phonedude_mln/status/783721103810105344"></iframe>
+
+<iframe border=0 frameborder=0 height=150 width=300 
+ src="http://twitframe.com/show?url=https://twitter.com/weiglemc/status/783675741959516160"></iframe>
+
+ 
+<iframe border=0 frameborder=0 height=150 width=300 
+ src="http://twitframe.com/show?url=https://twitter.com/JohnOlpoet/status/783728113138229250"></iframe>
+
+
+<iframe border=0 frameborder=0 height=150 width=300  
+ src="http://twitframe.com/show?url=https://twitter.com/phonedude_mln/status/783721103810105344"></iframe>
+
+<iframe border=0 frameborder=0 height=150 width=300 
+ src="http://twitframe.com/show?url=https://twitter.com/weiglemc/status/783675741959516160"></iframe>
