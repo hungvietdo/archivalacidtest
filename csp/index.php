31090610b24a7bddf0c9bd15d02e6bae37723af6
hungdoodu <do.viethung@gmail.com>
2016-11-16 12:00:55 -0500

    inline csp

--- csp/index.php
+++ csp/index.php
@@ -1,12 +1,19 @@
-<?php header("Content-Security-Policy: img-src 'https://www.google.com'");?>
+<meta http-equiv="Content-Security-Policy" 
+
+content="
+img-src 'self' http://www.google.com;
+script-src 'self' https://ajax.googleapis.com 'unsafe-inline';
+
+">
 <html>
 <head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript">
 
 function myFunction()
 {
   var filename = "img.php";
-  var leaking= "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
+  var leaking= "http://www.cs.odu.edu/~hdo/cs697/acidtest/csp/googlelogo_color_272x92dp.png";
   document.getElementById("image").src=filename;
   document.getElementById("leakingimage").src=leaking;
 }
@@ -24,22 +31,16 @@ function myFunction()
   <img id="image" src="" height='50'>
 </div>
 <div>
-  Leaking image (with js)
+   image (with js)
 </div>
 <div>
   <img id="leakingimage" src="" height='50'>
 </div>
 <div>
-  Leaking image (direct link)
+  Direct image (direct link)
 </div>
 <div>
   <img id="leakingimage_direct" src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" height='50'>
 </div>
-<script type="text/javascript">
-  var filename = "img.php";
-  var leaking= "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
-  document.getElementById("image").src=filename;
-  document.getElementById("leakingimage").src=leaking;
-</script>
 </body>
 </html>
