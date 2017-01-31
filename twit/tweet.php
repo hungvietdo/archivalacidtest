568d6c57b3f6d9a63a899d8c3d12eaf894f3598c
Hung Do <hung.do@dominionenterprises.com>
2017-01-31 13:20:10 -0500

    Working on tweet achive issue with internet archive

--- twit/tweet.php
+++ twit/tweet.php
@@ -1,6 +1,10 @@
 <html>
+<head>
+<link rel="stylesheet" href="tweet.3a5bba37d8a97ff1a6185653efe28c38.light.ltr.css">
+
+</head>
     <body>
 <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Hey <a href="https://twitter.com/weiglemc">@weiglemc</a> <a href="https://twitter.com/phonedude_mln">@phonedude_mln</a>. You were chosen as Cover Models of our today&#39;s Universitiy Lunch Lecture 😉. <a href="https://t.co/1QAaYrbosh">pic.twitter.com/1QAaYrbosh</a></p>&mdash; Michael A. Herzog (@maherzog) <a href="https://twitter.com/maherzog/status/822004794869116929">January 19, 2017</a></blockquote>
-<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<script src="w.js" charset="utf-8"></script>
     </body>
 </html>
