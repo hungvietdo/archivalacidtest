cdd81cfd3fc496b7c750a64bdd6cd758c98494f8
hungdoodu <do.viethung@gmail.com>
2017-02-14 15:58:03 -0500

    Update x-frame-option

--- csp/x-frame/test.php
+++ csp/x-frame/test.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <iframe src="frame.php" height="200" width="300"></iframe>
-<iframe src="http://www.google.com" height="200" width="300"></iframe>
+<iframe src="http://www.google.com" height="400" width="600"></iframe>
 
 </body>
 </html>
