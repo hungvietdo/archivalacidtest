76a762e2800549556832642d9934d5336af00be7
Hung Do <hung.do@dominionenterprises.com>
2017-03-12 22:14:14 -0400

    header meta and php

---
+++ csp/simpleheader.php
@@ -0,0 +1,14 @@
+<?php 
+header("Content-Security-Policy: www.google.com");
+?>
+<html>
+<head>
+
+</head>
+<body>
+    <div>
+        <h4>Direct image (https)</h4><br>
+    <img id="image_http" src="http://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" height='50'>
+    </div>
+</body>
+</html>
---
+++ csp/simpleheader_meta.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Security-Policy" content=" img-src www.google.com">
+
+</head>
+<body>
+    <div>
+        <h4>Direct image (https)</h4><br>
+    <img id="image_http" src="http://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" height='50'>
+    </div>
+</body>
+</html>
