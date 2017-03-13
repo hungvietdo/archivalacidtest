ab681d869fa3b7770450f836378b0d5440e3ea6d
hungdoodu <do.viethung@gmail.com>
2017-03-12 23:17:01 -0400

    fix domain name ODU

--- csp/simpleheader.php
+++ csp/simpleheader.php
@@ -1,5 +1,5 @@
 <?php 
-header("Content-Security-Policy: www.odu.edu");
+header("Content-Security-Policy: www.cs.odu.edu");
 ?>
 <html>
 <head>
@@ -7,7 +7,7 @@ header("Content-Security-Policy: www.odu.edu");
 </head>
 <body>
     <div>
-        <h4>Direct image (https)</h4><br>
+        <h4>Image from ODU webserver with header response from PHP.</h4><br>
     <img id="image_http" src="http://www.cs.odu.edu/~hdo/images/odu.png" height='50'>
     </div>
 </body>
--- csp/simpleheader_meta.php
+++ csp/simpleheader_meta.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-<meta http-equiv="Content-Security-Policy" content=" img-src www.odu.edu">
+<meta http-equiv="Content-Security-Policy" content=" img-src www.cs.odu.edu">
 </head>
 <body>
     <div>
-        <h4>Direct image (https)</h4><br>
+        <h4>Image from ODU web server with meta tag.</h4><br>
     <img id="image_http" src="http://www.cs.odu.edu/~hdo/images/odu.png" height='50'>
     </div>
 </body>
