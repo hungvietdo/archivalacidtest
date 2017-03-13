dbecfc1de686544626059f035570b80cc4738cba
Hung Do <hung.do@dominionenterprises.com>
2017-03-12 22:14:14 -0400

    header meta and php

---
+++ csp/simpleheader_meta.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Security-Policy" content=" img-src www.odu.edu">
+
+</head>
+<body>
+    <div>
+        <h4>Direct image (https)</h4><br>
+    <img id="image_http" src="http://www.cs.odu.edu/~hdo/images/odu.png" height='50'>
+    </div>
+</body>
+</html>
