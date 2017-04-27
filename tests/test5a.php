502c10aeb51ba0f0111faa0e235976aa63ad000d
hungdoodu <do.viethung@gmail.com>
2017-04-27 12:16:10 -0400

    Update link for leakage test

--- tests/test5a.php
+++ tests/test5a.php
@@ -3,7 +3,7 @@
         <td rowspan="2" style="vertical-align: middle;">Test 5</td>
         <td> Relative image  </td>
         <td>
-            <img id="originimage" src="img_leak.php" width="300">
+            <img id="originimage" src="http://www.cs.odu.edu/~hdo/acidtest/img_leak.php" width="300">
         </td>
         <td rowspan="2" style="vertical-align: middle;">
             <div class="text-primary" id="pass"></div>
